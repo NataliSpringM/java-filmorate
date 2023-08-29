5665df9dd8771bef2bdaec9bf2afa86cdb932b6d
Natalia <NataliSpringM@yandex.ru>
2023-08-29 16:38:08 +0300

    fix: fix codestyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/RatingMpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/RatingMpaStorage.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface RatingMpaStorage {
 
     // хранение информации о рейтингах фильмов
-    List<Mpa> listRatingMpa (); // получение списка рейтингов в системе MPA
+    List<Mpa> listRatingMpa(); // получение списка рейтингов в системе MPA
 
     Mpa getRatingMpaById(Integer id); // получение значение рейтинга MPA по id
 }
